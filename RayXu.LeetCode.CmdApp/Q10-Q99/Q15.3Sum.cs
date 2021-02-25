b4c41e772f81f3681ca5b0e86235d791c3ae4eca
Ray Xu <qihxu@microsoft.com>
2021-02-25 15:15:11 +0800

    add Q15.3Sum, Q16.3SumClosest

---
+++ RayXu.LeetCode.CmdApp/Q10-Q99/Q15.3Sum.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayXu.LeetCode.CmdApp
+{
+    partial class LeetCode
+    {
+        //Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+        //Notice that the solution set must not contain duplicate triplets.
+
+        //Example 1:
+        //Input: nums = [-1,0,1,2,-1,-4]
+        //Output: [[-1,-1,2],[-1,0,1]]
+
+        //Example 2:
+        //Input: nums = []
+        //Output: []
+
+        //Example 3:
+        //Input: nums = [0]
+        //Output: []
+
+        //Constraints:
+        //0 <= nums.length <= 3000
+        //-105 <= nums[i] <= 105
+
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            IList<IList<int>> results = new List<IList<int>>();
+
+            for (int left = 0; left < nums.Length - 2; left++)
+            {
+                if (left > 0 && nums[left] == nums[left - 1])
+                {
+                    continue;
+                }
+
+                for (int mid = left + 1, right = nums.Length - 1; mid < right;)
+                {
+                    int currentValue = nums[left] + nums[right] + nums[mid];
+                    if (currentValue == 0)
+                    {
+                        results.Add(new List<int> { nums[left], nums[mid], nums[right] });
+                        while (mid < right && nums[mid] == nums[mid + 1])
+                        {
+                            mid++;
+                        }
+
+                        while (mid < right && nums[right] == nums[right - 1])
+                        {
+                            right--;
+                        }
+
+                        mid++;
+                        right--;
+
+                    }
+                    else if (currentValue > 0)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        mid++;
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ RayXu.LeetCode.CmdApp/Q10-Q99/Q16.3SumClosest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayXu.LeetCode.CmdApp
+{
+    partial class LeetCode
+    {
+        //Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.Return the sum of the three integers.You may assume that each input would have exactly one solution.
+
+        //Example 1:
+        //Input: nums = [-1,2,1,-4], target = 1
+        //Output: 2
+        //Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+        //Constraints:
+        //3 <= nums.length <= 10^3
+        //-10^3 <= nums[i] <= 10^3
+        //-10^4 <= target <= 10^4
+
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            int dif = int.MaxValue, reslut = 0;
+
+            for (int left = 0; left < nums.Length - 2; left++)
+            {
+                if (left > 0 && nums[left] == nums[left - 1])
+                {
+                    continue;
+                }
+
+                for (int mid = left + 1, right = nums.Length - 1; mid < right;)
+                {
+                    int sum = nums[left] + nums[mid] + nums[right];
+
+                    if (sum == target)
+                    {
+                        return target;
+                    }
+                    else
+                    {
+                        int abs = Math.Abs(target - sum);
+                        if (abs < dif)
+                        {
+                            dif = abs;
+                            reslut = sum;
+                        }
+
+                        if (sum > target)
+                        {
+                            right--;
+                        }
+                        else
+                        {
+                            mid++;
+                        }
+                    }
+                }
+            }
+
+            return reslut;
+        }
+    }
+}
